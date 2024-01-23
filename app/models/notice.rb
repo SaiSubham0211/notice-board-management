e29ed59b03ccf4661522f16b6cba77e2affdaf28
SaiSubham0211 <patnaiksubham18@gmail.com>
2024-01-23 11:40:14 +0530

    Modified Notice

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,6 +1,6 @@
 class Notice < ApplicationRecord
 	validates :topic, presence: true, length: { minimum: 10, maximum: 50 }
-	validates :content, presence: true, length: { minimum: 50, maximum: 500 }
+	validates :content, presence: true, length: { minimum: 50, maximum: 5000 }
 	validates :date, presence: true
 	validates :Ref_No, presence: true
 end
